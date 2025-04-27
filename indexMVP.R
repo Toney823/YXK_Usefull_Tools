4d8445e51b5ede72bdc9c7944099aa8eaca183f3
KyleYu <33961678+Toney823@users.noreply.github.com>
2025-04-27 17:28:13 +0800

    Update indexMVP.R

--- indexMVP.R
+++ indexMVP.R
@@ -7,13 +7,14 @@
 
 library('rMVP')
 
+args <- commandArgs(trailingOnly=TRUE)
+
+cat("Arguments received:", args, "\n")
 
 if (length(args) < 2) {
-  cat("\nThis script is for perparing the index file for rMVP. \n")
+  cat("\nThis script is for preparing the index file for rMVP. \n")
   cat("Usage: Rscript indexMVP.R <vcf:STR> <IndexName:STR>\n")
-  q(status = 1) 
+  quit(status = 1) 
 }
 
-args <- commandArgs(trailingOnly=TRUE)
-setwd(getwd())
-MVP.Data(fileVCF=args[1], fileKin=TRUE, filePC=TRUE, out=args[2]) 
+MVP.Data(fileVCF=args[1], fileKin=TRUE, filePC=TRUE, out=args[2])
